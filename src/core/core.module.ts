1c4bf7ad86645835f57559ded98e2623f5063cd7
Maksym Marchenko <mmarchenko@antaeustravel.com>
2026-05-05 16:00:37 +0300

    feat(config): add global configuration module and update core module imports

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ConfigurationModule } from './config/configuration.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [ConfigurationModule, DatabaseModule],
 })
 export class CoreModule {}
